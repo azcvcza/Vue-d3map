b063c17826fc492a8e81f301247efbb28bebad61
韦粤信 <283940802@qq.com>
2019-04-28 23:16:21 +0800

    feat: successfully run a map

--- vue-d3/src/App.vue
+++ vue-d3/src/App.vue
@@ -1,23 +1,21 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
-
 <script>
+import MapViewer from './components/HelloWorld.vue';
 export default {
-  name: 'App'
+  name: 'app',
+  render () {
+    return (
+      <MapViewer />
+    );
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  cursor: default;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- vue-d3/src/components/HelloWorld.vue
+++ vue-d3/src/components/HelloWorld.vue
@@ -1,113 +1,194 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
-</template>
+
 
 <script>
+const new_center = [23.2203973, 108.8061526];
+const d3 = {
+  ...require('d3-geo'),
+  ...require('d3-tile'),
+};
+const MIN_ZOOM = 10;
+const MAX_ZOOM = 27;
 export default {
-  name: 'HelloWorld',
+  props: {
+    center: {
+      type: Array,
+      default: () => [ 10,23.2204],
+    },
+    initialZoom: {
+      type: [Number, String],
+      default: 20,
+    },
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      width: 0,
+      height: 0,
+      translateX: 0,
+      translateY: 0,
+      touchStarted: false,
+      touchLastX: 0,
+      touchLastY: 0,
+      zoom: +this.initialZoom,
+      scale: 1 << +this.initialZoom,
+    };
+  },
+  computed: {
+    projection () {
+      return d3.geoMercator()
+        .scale(+this.scale / (2 * Math.PI))
+        .translate([this.translateX, this.translateY])
+        .center(this.center)
+      ;
+    },
+    tiles () {
+      return d3.tile()
+        .size([this.width, this.height])
+        .scale(+this.scale)
+        .translate(this.projection([0, 0]))()
+      ;
+    },
+  },
+  watch: {
+    zoom (zoom, prevZoom) {
+      const k = zoom - prevZoom > 0 ? 2 : .5;
+      this.scale = 1 << zoom;
+      this.translateY = this.height / 2 - k * (this.height / 2 - this.translateY);
+      this.translateX = this.width / 2 - k * (this.width / 2 - this.translateX);
+    },
+  },
+  mounted () {
+    const rect = this.$el.getBoundingClientRect();
+    this.width = rect.width;
+    this.height = rect.height;
+    this.translateX = this.width / 2;
+    this.translateY = this.height / 2;
+  },
+  methods: {
+    onTouchStart (e) {
+      this.touchStarted = true;
+      this.touchLastX = e.clientX;
+      this.touchLastY = e.clientY;
+    },
+    onTouchEnd () {
+      this.touchStarted = false;
+    },
+    onTouchMove (e) {
+      if (this.touchStarted) {
+        this.translateX = this.translateX + e.clientX - this.touchLastX;
+        this.translateY = this.translateY + e.clientY - this.touchLastY;
+        this.touchLastX = e.clientX;
+        this.touchLastY = e.clientY;
+      }
+    },
+    zoomIn () {
+      this.zoom = Math.min(this.zoom + 1, MAX_ZOOM);
+    },
+    zoomOut () {
+      this.zoom = Math.max(this.zoom - 1, MIN_ZOOM);
+    },
+  },
+  render () {
+    if (this.width <= 0 || this.height <= 0) {
+      return <div class="map" />;
     }
-  }
-}
+    return (
+      <div class="map">
+        <div class="map__controls">
+          <button
+            class="map__button"
+            disabled={this.zoom >= MAX_ZOOM}
+            onClick={this.zoomIn}
+          >+</button>
+          <button
+            class="map__button"
+            disabled={this.zoom <= MIN_ZOOM}
+            onClick={this.zoomOut}
+          >-</button>
+        </div>
+        <svg
+          viewBox={`0 0 ${this.width} ${this.height}`}
+          onMousedown={this.onTouchStart}
+          onMousemove={this.onTouchMove}
+          onMouseup={this.onTouchEnd}
+          onMouseleave={this.onTouchEnd}
+        >
+          <g>
+            {this.tiles.map((t, i) => (
+              
+              <image
+                class="map__tile"
+                key={`${t.x}_${t.y}_${t.z}_${i}`}
+                xlinkHref={`https://a.tile.openstreetmap.org/${t.z}/${t.x}/${t.y}.png `}
+                x={(t.x + this.tiles.translate[0]) * this.tiles.scale}
+                y={(t.y + this.tiles.translate[1]) * this.tiles.scale}
+                width={this.tiles.scale}
+                height={this.tiles.scale}
+              />
+            ))}
+          </g>
+        </svg>
+        <div class="map__copyright">
+          ©&nbsp;
+          <a
+            href="https://www.openstreetmap.org/copyright"
+            target="_blank"
+          >OpenStreetMap&nbsp;</a>
+          contributors
+        </div>
+      </div>
+    );
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.map {
+	position: relative;
+	width: 100%;
+	height: 100%;
+	font-family: Arial, sans, sans-serif;
+	
+	
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+.map .map__controls {
+		position: absolute;
+		left: 16px;
+		top: 16px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 56px;
+	}
+.map .map__button {
+		border: 0;
+		padding: 0;
+		width: 24px;
+		height: 24px;
+		line-height: 24px;
+		border-radius: 50%;
+		font-size: 18px;
+		background-color: #ffffff;
+		color: #343434;
+		box-shadow: 0 1px 4px rgba(0, 0, 0, 0.4);
+		
+	}
+  .map .map__button:hover,
+		:focus {
+			background-color: #eeeeee;
+		}
+		:disabled {
+			background-color: rgba(#eeeeee, 0.4);
+		}
+	.map .map__tile {
+		pointer-events: none;
+	}
+	.map .map__copyright {
+		position: absolute;
+		bottom: 8px;
+		right: 8px;
+		padding: 2px 4px;
+		background-color: rgba(#ffffff, 0.6);
+		font-size: 14px;
+	}
 </style>
